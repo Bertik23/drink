f620d2571a16cc855e63a9eff5b2c98e2528d790
bertik23 <bertikxxiii@gmail.com>
2020-04-23 11:42:52 +0200

    Making it cross platform

--- main.py
+++ main.py
@@ -1,39 +1,39 @@
 import time
 import ctypes  # An included library with Python install.
+from configparser import ConfigParser
+import winsound #winsound.PlaySound('sound.wav', winsound.SND_FILENAME)
+
+config = ConfigParser()
+languages = ConfigParser()
+config.read("config.ini")
+languages.read("languages.ini")
+
 def Mbox(title, text, style):
     return ctypes.windll.user32.MessageBoxW(0, text, title, style)
-##  Styles:
-##  0 : OK
-##  1 : OK | Cancel
-##  2 : Abort | Retry | Ignore
-##  3 : Yes | No | Cancel
-##  4 : Yes | No
-##  5 : Retry | No 
-##  6 : Cancel | Try Again | Continue
-import winsound
-#winsound.PlaySound('sound.wav', winsound.SND_FILENAME)
-languagesFile = open("language.txt","r")
-l = languagesFile.readlines()
-for line in l:
-	#print(line)
-	l[l.index(line)] = line[:-1]
-	#print(line)
-#print(l)
-for i in range(len(l)-1):
-	#print(l[i].split("|"))
-	if l[i+1].split("|")[0] == l[0]:
-		language = l[i+1].split("|")[0]
-		mboxtitle = l[i+1].split("|")[1]
-		mboxtext = l[i+1].split("|")[2]
+    ##  Styles:
+    ##  0 : OK
+    ##  1 : OK | Cancel
+    ##  2 : Abort | Retry | Ignore
+    ##  3 : Yes | No | Cancel
+    ##  4 : Yes | No
+    ##  5 : Retry | No
+    ##  6 : Cancel | Try Again | Continue
+
+def updateLanguage():
+    config.read("config.ini")
+    languages.read("languages.ini")
+    language = config.get("main","language")
+    mboxtitle = languages.get("language","title")
+    mboxtext = language.get("language","text")
 
 def run():
 	global timer
 	winsound.PlaySound(f'sounds/{language}.wav', winsound.SND_FILENAME)
 	Mbox(mboxtitle,mboxtext,0)
 	winsound.PlaySound(f'sounds/{language}.wav', winsound.SND_FILENAME)
-
-
-timer = 1_800#s	
-while True:
+updateLanguage()
+print(language,mboxtext,mboxtitle)
+timer = 1_800#s
+while True == False:
 	run()
 	time.sleep(timer)
