2d76b74819cc665d178aaef6ea9fd92f5c7058fe
bertik23 <bertikxxiii@gmail.com>
2020-04-24 10:18:02 +0200

    It is cross platform now!

--- main.py
+++ main.py
@@ -1,39 +1,33 @@
 import time
-import ctypes  # An included library with Python install.
+from pymsgbox import alert
 from configparser import ConfigParser
-import winsound #winsound.PlaySound('sound.wav', winsound.SND_FILENAME)
+from boombox import BoomBox
 
 config = ConfigParser()
 languages = ConfigParser()
 config.read("config.ini")
 languages.read("languages.ini")
 
-def Mbox(title, text, style):
-    return ctypes.windll.user32.MessageBoxW(0, text, title, style)
-    ##  Styles:
-    ##  0 : OK
-    ##  1 : OK | Cancel
-    ##  2 : Abort | Retry | Ignore
-    ##  3 : Yes | No | Cancel
-    ##  4 : Yes | No
-    ##  5 : Retry | No
-    ##  6 : Cancel | Try Again | Continue
-
-def updateLanguage():
+def updateValues():
+    global language, mboxtitle, mboxtext, sound, timer
     config.read("config.ini")
     languages.read("languages.ini")
     language = config.get("main","language")
-    mboxtitle = languages.get("language","title")
-    mboxtext = language.get("language","text")
+    mboxtitle = languages.get(language,"title")
+    mboxtext = languages.get(language,"text")
+    sound = BoomBox(f"sounds/{language}.wav")
+    timer = config.get("main","remindingInterval")
 
 def run():
-	global timer
-	winsound.PlaySound(f'sounds/{language}.wav', winsound.SND_FILENAME)
-	Mbox(mboxtitle,mboxtext,0)
-	winsound.PlaySound(f'sounds/{language}.wav', winsound.SND_FILENAME)
-updateLanguage()
-print(language,mboxtext,mboxtitle)
-timer = 1_800#s
+    updateValues()
+	sound.play()
+	alert(title = mboxtitle, text = mboxtext, button = "OK")
+	sound.play
+
+updateValues()
+
+run()
+
 while True == False:
 	run()
 	time.sleep(timer)
